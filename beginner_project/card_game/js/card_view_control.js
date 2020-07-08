698cf4685f5f09934191a7e13d2ee9cc990d63e4
Gilbert.Wang(王彙智) <gilbert.wang@vivotek.com>
2020-07-08 11:13:25 +0800

    refactor card game controller

--- beginner_project/card_game/js/card_view_control.js
+++ beginner_project/card_game/js/card_view_control.js
@@ -1,3 +1,93 @@
+class Card {
+    constructor(cardIndex, matchIndex) {
+        this._cardIndex = cardIndex;
+        this._matchIndex = matchIndex;
+        this._isFlipUp = false;
+        this._isMatch = false;
+    }
+
+    get index() {
+        return this._cardIndex;
+    }
+    get matchIndex() {
+        return this._matchIndex;
+    }
+
+    set isFlipUp(isFlipUp) {
+        this._isFlipUp = isFlipUp;
+    }
+
+    get isFlipUp() {
+        return this._isFlipUp;
+    }
+
+    set isMatched(isMatch) {
+        this._isMatch = isMatch;
+    }
+
+    get isMatched() {
+        return this._isMatch;
+    }
+
+    isMatchWith(card) {
+        return card.index === this._matchIndex;
+    }
+}
+
+class CardGame {
+    constructor(numberCards) {
+        this._numberCards = numberCards;
+        this._previousChoose = null;
+        this._arrCards = [];
+    }
+
+
+
+    init() {
+        let indexSet = new Set(Array.from({ length: this._numberCards }, (_, idx) => idx));
+        this._arrCards = Array.from({ length: this._numberCards }, (_, index) => {
+
+            let indexArr = Array.from(indexSet);
+            let matchIndex = 0;
+
+            do {
+                let randIndex = Math.floor(Math.random() * indexArr.length);
+                matchIndex = indexArr[randIndex];
+            } while (matchIndex === index)
+
+            let newCard = new Card(index, matchIndex);
+            indexSet.delete(matchIndex);
+
+            console.log(`${newCard.index} match with ${newCard.matchIndex}`);
+            return newCard;
+        });
+    }
+
+
+    chooseCard(index) {
+        let cardChosen = _arrCards[index];
+
+        //card is first chose
+        if (!cardChosen.isFlipUp && !_previousChose) {
+            cardChosen.isFlipUp = true;
+            _previousChose = cardChosen;
+        }
+
+        // card is second chose
+        else if (!cardChosen.isFlipUp && _previousChose) {
+            cardChosen.isFlipUp = true;
+
+            if (cardChosen.isMatchWith(_previousChose)) {
+                cardChosen.isMatch = _previousChose.isMatch = true;
+            }
+            else {
+                cardChosen.isFlipUp = _previousChose.isFlipUp = false;
+            }
+            _previousChose = null;
+        }
+    }
+}
+
 class CardViewControl {
     constructor() {
         this._DOMString = {
@@ -28,12 +118,13 @@ class CardViewControl {
         let gameBoardElement = document.querySelector(this._DOMString.gameBoard);
         gameBoardElement.innerHTML = '';
 
-        for (let idx = 0; idx < this._cardNumber; ++idx){
+        for (let idx = 0; idx < this._cardNumber; ++idx) {
             let cardElement = this.createCard();
+            cardElement.id = 'card-' + idx
             gameBoardElement.appendChild(cardElement);
             this._gridCards.push(cardElement);
         }
-        
+
     }
 
 
@@ -51,31 +142,35 @@ class CardViewControl {
 
 class EventHandler {
 
-    constructor(viewControl) {
-        this._viewControl = viewControl;
+    constructor(viewController, gameController) {
+        this._viewController = viewController;
+        this._gameController = gameController;
     }
 
     setUpCardEvents() {
-        this._viewControl.cardElements.forEach((cardElement, idx)=>{ 
-            cardElement.addEventListener('click', ()=>{
+        this._viewController.cardElements.forEach((cardElement, idx) => {
+            cardElement.addEventListener('click', () => {
                 //TODO flip card event
-                console.log(`Card No.${idx} is clicked`);
+
             });
         });
     }
 
     setUpNewGameListener() {
-        this._viewControl.newButtonElement.addEventListener('click', () => {
-            this._viewControl.init();
+        this._viewController.newButtonElement.addEventListener('click', () => {
+            this._viewController.init();
+            this._gameController.init();
             this.setUpCardEvents();
         })
     }
 
-    
+
 }
 
-let viewControler = new CardViewControl();
-let eventHandler = new EventHandler(viewControler);
+let numberCards = 16;
+let viewController = new CardViewControl();
+let gameController = new CardGame(numberCards);
+let eventHandler = new EventHandler(viewController, gameController);
 
 eventHandler.setUpNewGameListener();
 
